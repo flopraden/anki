18ebf45f29ef628c6a8b9b065e1ff03470af29d8
Arthur Milchior <Arthur@Milchior.fr>
2019-06-17 17:45:14 +0200

    Allow to open the same window multiple time

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -271,8 +271,10 @@ Not currently enabled; click the sync button in the main window to enable."""))
         value = getattr(widget, function)()
         storeValue[name] = value
 
-    extraOptions = [
+    extraOptions = ([
     ]
+                    +[(f"{window}MultipleTime", True) for window in ["AddCards", "EditCurrent", "Browser", "OtherWindows"]]#those are the windows name, thus with caps
+    )
 
     def setupExtra(self):
         """Set in the GUI the preferences related to add-ons
