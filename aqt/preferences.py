8912ace5f17ab27ecfc8334d6b78bf1768f87992
Arthur Milchior <Arthur@Milchior.fr>
2019-06-17 15:15:33 +0200

    Consider name when changing type of a note

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -272,6 +272,7 @@ Not currently enabled; click the sync button in the main window to enable."""))
         storeValue[name] = value
 
     extraOptions = [
+        ("preserveName", True),
     ]
 
     def setupExtra(self):
