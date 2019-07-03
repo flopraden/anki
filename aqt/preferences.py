1a70d225ce26d86cbe0acee23e452aa4cdd50447
Arthur Milchior <Arthur@Milchior.fr>
2019-07-03 22:11:20 +0200

    Compile latex asap

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -272,6 +272,7 @@ Not currently enabled; click the sync button in the main window to enable."""))
         storeValue[name] = value
 
     extraOptions = [
+        ("compileLaTeX", ),
     ]
 
     def setupExtra(self):
