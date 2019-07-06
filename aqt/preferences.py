6f487b56c91735f2c8e549a1794f9d51fc429fb3
Arthur Milchior <Arthur@Milchior.fr>
2019-07-06 04:34:29 +0200

    Batch edit

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -272,6 +272,7 @@ Not currently enabled; click the sync button in the main window to enable."""))
         storeValue[name] = value
 
     extraOptions = [
+        ("newLineInBatchEdit",),
     ]
 
     def setupExtra(self):
