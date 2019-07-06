da41cbac278d1c188e996219d2f84591b2d62aeb
Arthur Milchior <Arthur@Milchior.fr>
2019-07-06 18:40:05 +0200

    Allow empty first field

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -272,6 +272,8 @@ Not currently enabled; click the sync button in the main window to enable."""))
         storeValue[name] = value
 
     extraOptions = [
+        ("allowEmptyFirstField", ),
+        ("multipleNoteWithSameFirstFieldInImport", ),
     ]
 
     def setupExtra(self):
